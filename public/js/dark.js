83bbbafc1c9eaf0120b28f8338e7a718721d6331
Geoffrey le Meur <geoffrey.lemeur@gmail.com>
2024-06-21 16:04:43 +0200

    feat: aria-pressed support on darkmode toggle

--- public/js/dark.js
+++ public/js/dark.js
@@ -1,31 +1,36 @@
 // DARK MODE //
-
+const darkModeToggleButton = document.getElementById('dark-mode-toggle')
 // View Transitions - sets or persist theme in local storage after swap to avoid FOUC
 document.addEventListener('astro:after-swap', () => {
     detectColorScheme()
 })
-  
+
 // View Transitions - runs on page load
 document.addEventListener('astro:page-load', () => {
-    
-    detectColorScheme(); 
+
+    detectColorScheme();
 
     // add event listener to the dark mode button toggle
-    document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+    darkModeToggleButton.addEventListener('click', () => {
         // on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
         localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
     });
 })
-    
 
 // helper functions to toggle dark mode
 function enableDarkMode() {
     document.body.classList.add('dark-mode');
     localStorage.setItem('theme', 'dark');
+    ariaPressed(darkModeToggleButton)
 }
 function disableDarkMode() {
     document.body.classList.remove('dark-mode');
     localStorage.setItem('theme', 'light');
+    ariaPressed(darkModeToggleButton)
+}
+function ariaPressed(element) {
+    const isPressed = element.getAttribute('aria-pressed');
+    element.setAttribute('aria-pressed', isPressed === 'false' ? 'true' : 'false');
 }
 
 // determines a new users dark mode preferences
