c35bf2ff5163cea60df9c59f9dac45b2d8e02ab2
Geoffrey Le Meur <geoffrey.lemeur@gmail.com>
2024-10-14 21:37:40 +0200

    feat: added support for preloading images

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -10,3 +10,16 @@ export function formatDate(date) {
 export function getCurrentYear() {
   return new Date().getFullYear();
 }
+
+import { getImage } from "astro:assets";
+export async function getOptimizedImage(image) {
+  const optimizedImage = await getImage({
+    src: image,
+    format: "webp",
+  });
+
+  return optimizedImage
+}
+
+// Learn more agout the getImage() function here
+// https://docs.astro.build/en/guides/images/#generating-images-with-getimage
